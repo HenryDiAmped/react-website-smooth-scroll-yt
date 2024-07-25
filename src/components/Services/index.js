8c82ed4c49ded80f06de29e899903f9e5b9f0fad
HenryDiAmped <enciso26.2004@gmail.com>
2024-07-24 19:38:21 -0500

    Web terminada

---
+++ src/components/Services/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Icon1 from '../../images/svg-5.svg';
+import Icon2 from '../../images/svg-6.svg';
+import Icon3 from '../../images/svg-7.svg';
+import Icon4 from '../../images/svg-8.svg';
+import {
+    ServicesContainer,
+    ServicesH1,
+    ServicesWrapper,
+    ServicesCard,
+    ServicesIcon,
+    ServicesH2,
+    ServicesP,
+} from './ServicesElements';
+
+const Services = () => {
+    return (
+        <ServicesContainer id="services" >
+            <ServicesH1>Our Services</ServicesH1>
+            <ServicesWrapper>
+                <ServicesCard>
+                    <ServicesIcon src={Icon1} />
+                    <ServicesH2>Reduce expenses</ServicesH2>
+                    <ServicesP>We help reduce your fess and encrease your overall revenue.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon2} />
+                    <ServicesH2>Virtual Offices</ServicesH2>
+                    <ServicesP>You can access our platform online anuwhere in the world.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon3} />
+                    <ServicesH2>Premium Benefits</ServicesH2>
+                    <ServicesP>Unlock our special membership card that returns 5% cash back.</ServicesP>
+                </ServicesCard>
+                <ServicesCard>
+                    <ServicesIcon src={Icon4} />
+                    <ServicesH2>Financial advice</ServicesH2>
+                    <ServicesP>We offer personalized advice to help you manage your finances efficiently.</ServicesP>
+                </ServicesCard>
+            </ServicesWrapper>
+        </ServicesContainer>
+    )
+}
+
+export default Services
